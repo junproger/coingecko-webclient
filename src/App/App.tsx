07e6734140b845269e4fb8961152d24e2cc4b322
Junior Programmer <progphyton@gmail.com>
2023-02-22 23:37:39 +0300

    feat: add component input

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Button } from "@Components/Button";
 import { Card } from "@Components/Card";
+import { Input } from "@Components/Input";
 import { Loader } from "@Components/Loader";
 import { WithLoader } from "@Components/WithLoader";
 
@@ -12,6 +13,12 @@ const App: React.FC = () => {
     <div className={styleApp.app}>
       <Loader loading={true} />
       <WithLoader loading={true}>Page content</WithLoader>
+      <Input
+        disabled
+        value="What is Bitcoin?"
+        // eslint-disable-next-line no-console
+        onChange={(value: string) => console.log(value)}
+      />
       <Button loading={true} disabled>
         Cancel
       </Button>
---
+++ src/Shared/Components/Input/Input.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEvent, InputHTMLAttributes } from "react";
+
+import classNames from "classnames";
+
+import "@Styles/styleIndex.scss";
+import "./styleInput.scss";
+
+export interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> {
+  value: string;
+  disabled?: boolean | undefined;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+export const Input: React.FC<InputProps> = ({
+  value = "",
+  placeholder = "",
+  onChange,
+  className,
+  disabled = false,
+  ...attrib
+}) => {
+  const inputClasses = classNames(
+    "input",
+    { input_disabled: disabled },
+    className
+  );
+  const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    return onChange(event.target.value);
+  };
+  return (
+    <input
+      type="text"
+      value={value}
+      className={inputClasses}
+      placeholder={placeholder}
+      onChange={inputHandler}
+      disabled={disabled}
+      {...attrib}
+    />
+  );
+};
