78207649c4686c6ca43334c1a6a126f7db642265
Junior Programmer <progphyton@gmail.com>
2023-02-24 16:56:53 +0300

    feat: add coinmart styles

--- src/App/Pages/CoinMart/CoinMart.tsx
+++ src/App/Pages/CoinMart/CoinMart.tsx
@@ -3,14 +3,15 @@ import React from "react";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import styleCoinMart from "./styleCoinMart.module.scss";
 
 const CoinMart: React.FC = () => {
   return (
-    <>
+    <div className={styleCoinMart.coinmart}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 };
 
