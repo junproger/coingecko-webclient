f9ecd0395f3e7148895a3a7fb965b5d5719eb1b5
Junior Programmer <progphyton@gmail.com>
2023-02-24 20:10:16 +0300

    feat: add multidropdown to choose component

--- src/App/Pages/CoinMart/Components/Header/Components/Choose/Choose.tsx
+++ src/App/Pages/CoinMart/Components/Header/Components/Choose/Choose.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 
 import { LogoType } from "@Components/LogoType";
+import { MultiDropdown, Option } from "@Components/MultiDropdown";
 
 import styleChoose from "./styleChoose.module.scss";
 
 const Choose: React.FC = () => {
+  const options: Option[] = [
+    { key: "msk", value: "Москва" },
+    { key: "spb", value: "Санкт-Петербург" },
+    { key: "ekb", value: "Екатеринбург" },
+  ];
+
+  const pluralizeOptions = (elements: Option[]) =>
+    elements.map((elm: Option) => elm.key).join();
+
+  const [value, setValue] = React.useState<Option[]>([]);
+
   return (
     <div className={styleChoose.choose}>
       <LogoType children="Coins" />
-      Choose
+      <MultiDropdown
+        tabIndex={1}
+        value={value}
+        disabled={false}
+        options={options}
+        onChange={setValue}
+        placeHolder="Choose category"
+        pluralizeOptions={pluralizeOptions}
+      />
     </div>
   );
 };
