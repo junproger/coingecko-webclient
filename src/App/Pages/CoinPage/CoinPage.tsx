18be7e63df9e6e2804230215009c6b44ddd78c3e
Junior Programmer <progphyton@gmail.com>
2023-02-26 11:55:58 +0300

    feat: add coinpage component with fetching data

--- src/App/Pages/CoinPage/CoinPage.tsx
+++ src/App/Pages/CoinPage/CoinPage.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
+import { useFetchCoinInfo } from "@Hooks/useFetchCoinInfo";
+import { useParams } from "react-router-dom";
+
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import styleCoinPage from "./styleCoinPage.module.scss";
 
 const CoinPage: React.FC = () => {
+  const { idcoin } = useParams<{ idcoin: string }>();
+
+  const CoinInfo = useFetchCoinInfo(idcoin);
   return (
     <div className={styleCoinPage.coinpage}>
-      <Header />
-      <Main />
+      <Header coininfodata={CoinInfo} />
+      <Main description={CoinInfo?.description.en || ""} />
       <Footer />
     </div>
   );
