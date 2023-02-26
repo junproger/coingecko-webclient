08ec77b5ce3b336fbd890abd88228f482900491d
Junior Programmer <progphyton@gmail.com>
2023-02-26 17:42:17 +0300

    feat: add style to header

--- src/App/Pages/CoinPage/Components/Header/Header.tsx
+++ src/App/Pages/CoinPage/Components/Header/Header.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 import Prices from "./Components/Prices";
-import Refers from "./Components/Refers";
 import Titles from "./Components/Titles";
+import Values from "./Components/Values";
 import { ICoinInfoData } from "./Interfaces/ICoinInfoData";
+import styleHeader from "./styleHeader.module.scss";
 
 const Header: React.FC<ICoinInfoData> = ({ coininfodata }) => {
   return (
-    <header>
+    <header className={styleHeader.header}>
       <Titles coininfodata={coininfodata} />
       <Prices coininfodata={coininfodata} />
-      <Refers />
+      <Values coininfodata={coininfodata} />
     </header>
   );
 };
