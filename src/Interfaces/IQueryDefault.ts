cd38688860126c3786c782d607469e2a77b7bb67
Junior Programmer <progphyton@gmail.com>
2023-02-27 11:29:51 +0300

    refactor: querydefault as default working and template concept versions

--- src/Interfaces/IQueryDefault.ts
+++ src/Interfaces/IQueryDefault.ts
@@ -1,27 +1,8 @@
 export interface IQueryDefault {
-  uipath: string;
-  nquery: string;
-  tquery: string;
-  dquery: string;
-  origin: string;
-  apiver: string;
-  apidoc: string;
-  target: string;
-  getapi: string;
-  params: string;
+  hook: string;
+  control: null;
+  handler: null;
+  queries: string;
   request: string;
-  queryHandle: {
-    baseUrl: null;
-    basePage: null;
-    startUrl: null;
-    startPage: null;
-    currentUrl: null;
-    currentPage: null;
-    nextUrl: null;
-    nextPage: null;
-    prevUrl: null;
-    prevPage: null;
-    endUrl: null;
-    endPage: null;
-  };
+  apidata: null;
 }
