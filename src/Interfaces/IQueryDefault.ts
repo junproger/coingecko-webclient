9b8bbdc8b0b9fb556089cea49922cee48e587d85
Junior Programmer <progphyton@gmail.com>
2023-02-28 17:50:41 +0300

    refactor: querydefault and its interface with new data

--- src/Interfaces/IQueryDefault.ts
+++ src/Interfaces/IQueryDefault.ts
@@ -1,17 +1,21 @@
 export interface IQueryDefault {
+  api: string;
   hook: string;
+  datas: string;
   control: {
     type: string;
     path: string;
   };
   handler: null;
   query: string;
+  currency: string;
+  paging: string;
   params: {
+    required: string;
     vs_currency: string;
-    rep_page: number;
+    per_page: number;
     page: number;
   };
   urlreq: string;
   request: string;
-  api: string;
 }
--- src/Shared/Queries/queryDefault.ts
+++ src/Shared/Queries/queryDefault.ts
@@ -1,20 +1,24 @@
 import { IQueryDefault } from "src/Interfaces/IQueryDefault";
 
 export const queryDefault: IQueryDefault = {
+  api: "coins",
   hook: "market",
+  datas: "market",
   control: {
     type: "page",
     path: "./App/Pages/CoinMart",
   },
   handler: null,
-  query: "usd&per_page=10&page=1",
+  query: "usd",
+  currency: "usd",
+  paging: "&per_page=10&page=1",
   params: {
+    required: "vs_currency",
     vs_currency: "usd",
-    rep_page: 10,
+    per_page: 10,
     page: 1,
   },
-  urlreq: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=",
+  urlreq: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd",
   request:
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10&page=1",
-  api: "coins",
 };
