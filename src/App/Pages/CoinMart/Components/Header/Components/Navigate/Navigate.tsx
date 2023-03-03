a6d2d26f92d59ca74c4c7a9681e6980245a17465
Junior Programmer <progphyton@gmail.com>
2023-03-03 11:59:29 +0300

    feat: add useeffect for updating paging and state for stop point

--- src/App/Pages/CoinMart/Components/Header/Components/Navigate/Navigate.tsx
+++ src/App/Pages/CoinMart/Components/Header/Components/Navigate/Navigate.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Link, useParams } from "react-router-dom";
 
@@ -8,17 +8,27 @@ const Navigate: React.FC = () => {
   const { idpage } = useParams<{ idpage: string }>();
   const pageNum = parseInt(idpage || "1", 10);
 
+  const [getShow, setShow] = useState<boolean>(true);
   const [getPrev, setPrev] = useState<number>(pageNum - 1);
   const [getNext, setNext] = useState<number>(pageNum + 1);
 
+  useEffect(() => {
+    setPrev(pageNum - 1);
+    setNext(pageNum + 1);
+  }, [pageNum]);
+
   const handlePrev = () => {
+    setShow(true);
     if (getPrev < 1) return;
     setPrev((prev) => prev - 1);
     setNext((next) => next - 1);
   };
 
   const handleNext = () => {
-    if (getNext > 1232) return;
+    if (getNext >= 1231) {
+      setShow(false);
+      return;
+    }
     setPrev((prev) => prev + 1);
     setNext((next) => next + 1);
   };
@@ -38,16 +48,20 @@ const Navigate: React.FC = () => {
             &lt;prev
           </Link>
         ) : (
-          <span>&lt;prev</span>
+          <span className={styleNavigate.navigate__stop}>&lt;prev</span>
         )}
         <strong>{pageNum}</strong>
-        <Link
-          className={styleNavigate.navigate__links}
-          to={`/${getNext}`}
-          onClick={handleNext}
-        >
-          next&gt;
-        </Link>
+        {getShow ? (
+          <Link
+            className={styleNavigate.navigate__links}
+            to={`/${getNext}`}
+            onClick={handleNext}
+          >
+            next&gt;
+          </Link>
+        ) : (
+          <span className={styleNavigate.navigate__stop}>next&gt;</span>
+        )}
       </div>
     </nav>
   );
