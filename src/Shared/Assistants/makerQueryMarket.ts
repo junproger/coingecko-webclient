8c5072087ac50fd980b8bca9905f6f2ab8a89ecd
Junior Programmer <progphyton@gmail.com>
2023-03-03 10:08:20 +0300

    feat: add market and paging query assistants

---
+++ src/Shared/Assistants/makerQueryMarket.ts
@@ -0,0 +1,48 @@
+import { IQUERYMarket } from "@Interfaces/IQUERYMarket";
+
+export const makerQueryMarket = ([value, digit]: [
+  string,
+  number
+]): IQUERYMarket => {
+  const paging = digit;
+  const endpage = true;
+  const pageend = false;
+  const errors = "";
+  const queries = value;
+  const scheme = {
+    api: "coins",
+    hook: "market",
+    scheme: "market",
+    errors: "",
+    control: {
+      type: "page",
+      path: "./App/Pages/CoinMart",
+    },
+    handler: {
+      type: "useFetchMarket",
+      path: "@Hooks/useFetchMarket",
+    },
+    queries: queries,
+    currency: queries,
+    paging: paging,
+    params: {
+      pagination: "&per_page=10&page=1",
+      required: "vs_currency",
+      vs_currency: queries,
+      per_page: 10,
+      page: paging,
+    },
+    v3docs: "https://www.coingecko.com/api/documentations/v3",
+    urlreq:
+      "https://api.coingecko.com/api/v3/coins/markets?per_page=10&page=1&vs_currency=",
+    request: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${queries}&per_page=10&page=${paging}`,
+  };
+  return {
+    paging: paging,
+    endpage: endpage,
+    pageend: pageend,
+    errors: errors,
+    queries: queries,
+    scheme: scheme,
+  };
+};
