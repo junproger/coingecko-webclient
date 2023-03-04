852ec61070e0322c445f687fd79863aa19bd1ce2
Junior Programmer <progphyton@gmail.com>
2023-03-04 10:54:00 +0300

    feat: add data about of currencies to components

--- src/App/Pages/CoinPage/Components/Header/Components/Prices/Prices.tsx
+++ src/App/Pages/CoinPage/Components/Header/Components/Prices/Prices.tsx
@@ -1,31 +1,33 @@
 import React from "react";
 
 import { Loader, LoaderSize } from "@Components/Loader";
+import { currencySymbols } from "@Configs/currencySymbols";
+import { useParams } from "react-router-dom";
 
 import stylePrices from "./stylePrices.module.scss";
 import { ICoinInfoData } from "../../Interface/ICoinInfoData";
 
 const Prices: React.FC<ICoinInfoData> = ({ coininfodata }) => {
-  const current_price = coininfodata?.market_data.current_price.usd;
+  const { idcurr } = useParams() as { idcurr: string };
+
+  const current_price = coininfodata?.market_data.current_price[idcurr];
   const percentage_24h = coininfodata?.market_data.price_change_percentage_24h;
 
   const selectColor = (param: number | undefined) => {
     if (param && param >= 0) {
-      return "#21BF73";
+      return "prices__changes_up";
     } else {
-      return "#FB2173";
+      return "prices__changes_down";
     }
   };
 
   return coininfodata?.id ? (
     <div className={stylePrices.prices}>
-      <div className={stylePrices.prices__current}>${current_price}</div>
-      <div
-        className={stylePrices.prices__changes}
-        style={{
-          color: selectColor(percentage_24h),
-        }}
-      >
+      <div className={stylePrices.prices__current}>
+        {currencySymbols[idcurr]}
+        {current_price || 0}
+      </div>
+      <div className={stylePrices[selectColor(percentage_24h)]}>
         {percentage_24h}%
       </div>
     </div>
--- src/App/Pages/CoinPage/Components/Header/Components/Values/Values.tsx
+++ src/App/Pages/CoinPage/Components/Header/Components/Values/Values.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
+import { currencySymbols } from "@Configs/currencySymbols";
+import { useParams } from "react-router-dom";
+
 import styleValues from "./styleValues.module.scss";
 import { ICoinInfoData } from "../../Interface/ICoinInfoData";
 
 const Values: React.FC<ICoinInfoData> = ({ coininfodata }) => {
-  const market_cap = coininfodata?.market_data.market_cap.usd;
-  const fully_diluted = coininfodata?.market_data.fully_diluted_valuation.usd;
+  const { idcurr } = useParams() as { idcurr: string };
+
+  const market_cap = coininfodata?.market_data.market_cap[idcurr];
+  const fully_diluted =
+    coininfodata?.market_data.fully_diluted_valuation[idcurr];
   const circulating = coininfodata?.market_data.circulating_supply;
   const total_supply = coininfodata?.market_data.total_supply;
   const max_supply = coininfodata?.market_data.max_supply;
@@ -14,13 +20,19 @@ const Values: React.FC<ICoinInfoData> = ({ coininfodata }) => {
     <div className={styleValues.values}>
       <div className={styleValues.values__item}>
         <div className={styleValues.values__item_title}>Market Cap</div>
-        <div className={styleValues.values__item_value}>{market_cap}</div>
+        <div className={styleValues.values__item_value}>
+          {currencySymbols[idcurr]}
+          {market_cap || 0}
+        </div>
       </div>
       <div className={styleValues.values__item}>
         <div className={styleValues.values__item_title}>
           Fully Diluted Valuation
         </div>
-        <div className={styleValues.values__item_value}>{fully_diluted}</div>
+        <div className={styleValues.values__item_value}>
+          {currencySymbols[idcurr]}
+          {fully_diluted || 0}
+        </div>
       </div>
       <div className={styleValues.values__item}>
         <div className={styleValues.values__item_title}>Circulating Supply</div>
