ea21b5f77ab1fa499c044506fbeea9966079ddab
Junior Programmer <progphyton@gmail.com>
2023-03-04 12:13:18 +0300

    fix: remove errors routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import ApiPing from "./Pages/ApiPing";
 import CoinMart from "./Pages/CoinMart";
 import CoinPage from "./Pages/CoinPage";
-import Error from "./Pages/Error";
 import styleApp from "./styleApp.module.scss";
 
 const App: React.FC = () => {
@@ -14,11 +13,10 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<CoinMart />} />
         <Route path="/:idpage" element={<CoinMart />} />
-        <Route path="/coins" element={<Error />} />
         <Route path="/coins/:idcoin" element={<CoinPage />} />
         <Route path="/coins/:idcoin/:idcurr" element={<CoinPage />} />
         <Route path="/ping" element={<ApiPing />} />
-        <Route path="/*" element={<Error />} />
+        <Route path="/*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
