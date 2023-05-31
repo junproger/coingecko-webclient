96fee4b6913b9f9703d3f1455b651a73bce7fb92
Junior Programmer <progphyton@gmail.com>
2023-05-31 10:00:42 +0300

    fix: export for notice component

--- src/App/Pages/CoinMart/Components/Header/Components/Notice/Notice.tsx
+++ src/App/Pages/CoinMart/Components/Header/Components/Notice/Notice.tsx
@@ -2,12 +2,10 @@ import React from "react";
 
 import styleNotice from "./styleNotice.module.scss";
 
-interface INoticeWithError {
+export interface INoticeWithError {
   coinmarterror: string;
 }
 
-const Notice: React.FC<INoticeWithError> = ({ coinmarterror }) => {
+export const Notice: React.FC<INoticeWithError> = ({ coinmarterror }) => {
   return <div className={styleNotice.notice}>{coinmarterror}</div>;
 };
-
-export default Notice;
